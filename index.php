23d7fec128ba6c62d6760a362df40eb2ffaebd8c
Erwinn-Developpement <76838949+Erwinn-Developpement@users.noreply.github.com>
2022-03-06 18:14:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,73 @@
-<?php phpinfo(); ?>
 <!DOCTYPE html>
-<!-- Basic html -->
 <html lang="en">
-  <head>
-    <meta charset="utf-8"/>
-    <link rel="stylesheets" href="style.css">
-    <title>Disbot blog!</title>
-  </head>
-  <body>
-    <h1> Nouveautés de DisBot </h1>
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DisBot - Nouveautés</title>
+</head>
+<body>
+
+    <h1>News of DisBot</h1>
+
+
+    <div id="news">
+            <h2> Title</h2>
+        
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat fugit vel corrupti cumque enim laboriosam, animi non quidem incidunt, minima quae libero ut magni dicta saepe autem culpa ipsa dolorem.</p>
+        <p id="date">Add 13/05/2020 by Anonybot</p>
+    </div>
+  
+    
+</body>
 </html>
+
+
+<style>
+
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    h1 {
+        text-align: center;
+        margin-bottom: 100px;
+    }
+
+
+    #news {
+        width: 300px;
+        border: solid 2px rgba(27, 98, 156, 0.623);
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        border-radius: 2px;
+    }
+
+    #news h2 {
+        color: aliceblue;
+        height: 45px;
+        background: url(https://cdn.pixabay.com/photo/2020/05/07/04/01/digitization-5140071__340.jpg);
+        background-size: cover;
+        margin: 0;
+        padding-top: 5%;
+        border-bottom: solid 2px rgba(27, 98, 156, 0.623);
+    }
+
+    #img {
+        float: left;
+    }
+
+    #date {
+        font-size: 10px;
+        color: rgb(85, 84, 84);
+    }
+
+    #hr {
+        width: 100%;
+        height: 2px;
+        background-color: rgba(27, 98, 156, 0.623);
+    }
+
+
+</style>
